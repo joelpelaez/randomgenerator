c256c57c59e35f5a055b3ded343977f55752da7e
Joel Peláez Jorge <joelpelaez@gmail.com>
2012-07-21 15:34:40 -0500

    Add GPLv3 header to Java Code
    Signed-off-by: Joel Peláez Jorge <joelpelaez@gmail.com>

--- src/org/darksoft/android/randomgenerator/MainActivity.java
+++ src/org/darksoft/android/randomgenerator/MainActivity.java
@@ -1,3 +1,24 @@
+/*
+ * MainActivity.java - Main Code of RandomGenerator
+ *
+ * Copyright (C) 2012 Joel Pelaez Jorge <joelpelaez@gmail.com>
+ * 
+ * This file is part of RandomGenerator
+ * 
+ * RandomGenerator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RandomGenerator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RandomGenerator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.darksoft.android.randomgenerator;
 
 import android.os.Bundle;
